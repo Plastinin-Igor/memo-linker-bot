f4196e27d12482130f6f1f8a153f2b798ec730e2
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-12-03 17:44:47 +0300

    first commit

---
+++ src/main/java/ru/plastinin/memo_linker_bot/MemoLinkerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.plastinin.memo_linker_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoLinkerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoLinkerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/plastinin/memo_linker_bot/MemoLinkerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.plastinin.memo_linker_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoLinkerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
